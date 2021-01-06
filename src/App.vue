a40861006a8946c65c9e7c77f29a1179d4860ffe
wendy <lin.li@idiaoyan.com>
2021-01-06 17:02:11 +0800

    feat:first three demo;引入element ui

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button>展示three.js</el-button>
+    <div id="container"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import * as Three from 'three'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      camera:null,
+      scene:null,
+      renderer:null,
+      mesh:null
+    }
+  },
+  
+  methods:{
+    init:function(){
+      let container = document.getElementById('container')
+      
+      this.camera = new Three.PerspectiveCamera(70,container.clientWidth/container.clientHeight,0.01,10)
+      this.camera.position.z = 0.6
+
+      this.scene = new Three.Scene()
+      let geometry = new Three.BoxGeometry(0.2,0.2,0.2)
+      let material = new Three.MeshNormalMaterial()
+      this.mesh = new Three.Mesh(geometry,material)
+      this.scene.add(this.mesh)
+
+      this.renderer = new Three.WebGLRenderer({animate:true})
+      this.renderer.setSize(container.clientWidth,container.clientHeight)
+      container.appendChild(this.renderer.domElement)
+
+    },
+    animate:function(){
+      requestAnimationFrame(this.animate)
+      this.mesh.rotation.x += 0.01
+      this.mesh.rotation.y +=0.02
+      this.renderer.render(this.scene,this.camera)
+    }
+  },
+  mounted(){
+    this.init()
+    this.animate()
   }
 }
 </script>
@@ -24,5 +59,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#container{
+  height: 400px;
 }
 </style>
