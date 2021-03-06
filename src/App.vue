90a3116c6c62a4998f184924e8b8ba7ca7c5ee15
lin.li <lin.li@idiaoyan.com>
2021-03-06 11:06:34 +0800

    测试tag

--- src/App.vue
+++ src/App.vue
@@ -6,49 +6,53 @@
 </template>
 
 <script>
-import * as Three from 'three'
+import * as Three from "three";
 
 export default {
-  name: 'App',
-  data(){
+  name: "App",
+  data() {
     return {
-      camera:null,
-      scene:null,
-      renderer:null,
-      mesh:null
-    }
+      camera: null,
+      scene: null,
+      renderer: null,
+      mesh: null,
+    };
   },
-  
-  methods:{
-    init:function(){
-      let container = document.getElementById('container')
-      
-      this.camera = new Three.PerspectiveCamera(70,container.clientWidth/container.clientHeight,0.01,10)
-      this.camera.position.z = 0.6
 
-      this.scene = new Three.Scene()
-      let geometry = new Three.BoxGeometry(0.2,0.2,0.2)
-      let material = new Three.MeshNormalMaterial()
-      this.mesh = new Three.Mesh(geometry,material)
-      this.scene.add(this.mesh)
+  methods: {
+    init: function() {
+      let container = document.getElementById("container");
 
-      this.renderer = new Three.WebGLRenderer({animate:true})
-      this.renderer.setSize(container.clientWidth,container.clientHeight)
-      container.appendChild(this.renderer.domElement)
+      this.camera = new Three.PerspectiveCamera(
+        70,
+        container.clientWidth / container.clientHeight,
+        0.01,
+        10
+      );
+      this.camera.position.z = 0.6;
 
+      this.scene = new Three.Scene();
+      let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
+      let material = new Three.MeshNormalMaterial();
+      this.mesh = new Three.Mesh(geometry, material);
+      this.scene.add(this.mesh);
+
+      this.renderer = new Three.WebGLRenderer({ animate: true });
+      this.renderer.setSize(container.clientWidth, container.clientHeight);
+      container.appendChild(this.renderer.domElement);
     },
-    animate:function(){
-      requestAnimationFrame(this.animate)
-      this.mesh.rotation.x += 0.01
-      this.mesh.rotation.y +=0.02
-      this.renderer.render(this.scene,this.camera)
-    }
+    animate: function() {
+      requestAnimationFrame(this.animate);
+      this.mesh.rotation.x += 0.01;
+      this.mesh.rotation.y += 0.02;
+      this.renderer.render(this.scene, this.camera);
+    },
   },
-  mounted(){
-    this.init()
-    this.animate()
-  }
-}
+  mounted() {
+    this.init();
+    this.animate();
+  },
+};
 </script>
 
 <style>
@@ -60,7 +64,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-#container{
+#container {
   height: 400px;
 }
 </style>
