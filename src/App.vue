8a58776544c88165b75356bdb16c7b3877fe37d0
wendy <lin.li@idiaoyan.com>
2021-07-13 10:26:09 +0800

    demo

--- src/App.vue
+++ src/App.vue
@@ -2,65 +2,89 @@
   <div id="app">
     <el-button>展示three.js</el-button>
     <div id="container"></div>
+    <geometry-cube></geometry-cube>
   </div>
 </template>
 
 <script>
-import * as Three from 'three'
+import * as Three from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+import GeometryCube from "./components/GeometryCube/index.vue";
 
 export default {
-  name: 'App',
-  data(){
+  name: "App",
+  data() {
     return {
-      camera:null,
-      scene:null,
-      renderer:null,
-      mesh:null
-    }
+      scene: null,
+      camera: null,
+      renderer: null,
+      mesh: null,
+    };
   },
-  
-  methods:{
-    init:function(){
-      let container = document.getElementById('container')
-      
-      this.camera = new Three.PerspectiveCamera(70,container.clientWidth/container.clientHeight,0.01,10)
-      this.camera.position.z = 0.6
+  components: { GeometryCube },
 
-      this.scene = new Three.Scene()
-      let geometry = new Three.BoxGeometry(0.2,0.2,0.2)
-      let material = new Three.MeshNormalMaterial()
-      this.mesh = new Three.Mesh(geometry,material)
-      this.scene.add(this.mesh)
+  methods: {
+    init: function() {
+      let container = document.getElementById("container");
 
-      this.renderer = new Three.WebGLRenderer({animate:true})
-      this.renderer.setSize(container.clientWidth,container.clientHeight)
-      container.appendChild(this.renderer.domElement)
+      this.camera = new Three.PerspectiveCamera(
+        70,
+        container.clientWidth / container.clientHeight,
+        0.01,
+        10
+      );
+      this.camera.position.z = 1;
+      // this.camera.position.x = 1
+      // this.camera.position.y = 0.5
 
+      this.scene = new Three.Scene();
+      let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
+      let material = new Three.MeshNormalMaterial();
+      this.mesh = new Three.Mesh(geometry, material);
+      this.scene.add(this.mesh);
+
+      // 光源
+      let point = new Three.PointLight(0xffffff);
+      point.position.set(400, 200, 300);
+      this.scene.add(point);
+      var ambient = new Three.AmbientLight(0x444444);
+      this.scene.add(ambient);
+
+      this.renderer = new Three.WebGLRenderer({ animate: false });
+      this.renderer.setSize(container.clientWidth, container.clientHeight);
+      container.appendChild(this.renderer.domElement);
     },
-    animate:function(){
-      requestAnimationFrame(this.animate)
-      this.mesh.rotation.x += 0.01
-      this.mesh.rotation.y +=0.02
-      this.renderer.render(this.scene,this.camera)
-    }
+    animate: function() {
+      this.mesh.rotation.x += 0.01;
+      this.mesh.rotation.y += 0.02;
+      this.renderer.render(this.scene, this.camera);
+      requestAnimationFrame(this.animate);
+    },
+    control: function() {
+      new OrbitControls(this.camera, this.renderer.domElement);
+
+      // let controls = new OrbitControls(this.camera,this.renderer.domElement)
+      // controls.addEventListener('change',this.animate)
+    },
   },
-  mounted(){
-    this.init()
-    this.animate()
-  }
-}
+  mounted() {
+    this.init();
+    this.animate();
+    this.control();
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
-#container{
+#container {
   height: 400px;
 }
 </style>
---
+++ src/components/GeometryCube/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    three.js demo geometry cube
+  </div>
+</template>
+
+<script>
+import * as Three from "three";
+
+export default {
+  name: "GeometryCube",
+  data() {
+    return {
+      scene: null,
+      camera: null,
+      renderer: null,
+      mesh: null,
+    };
+  },
+  props: {},
+  components: {},
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.scene = new Three.Scene();
+
+      let geomerty = new Three.BoxGeometry(0.2, 0.2, 0.2);
+      let material = new Three.MeshNormalMaterial();
+      this.mesh = new Three.Mesh(geomerty, material);
+      this.scene.add(this.mesh);
+
+      this.camera = new Three.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+      this.camera.position.z = 400;
+
+       // 光源
+      let point = new Three.PointLight(0xffffff);
+      point.position.set(400, 200, 300);
+      this.scene.add(point);
+      var ambient = new Three.AmbientLight(0x444444);
+      this.scene.add(ambient);
+
+      this.renderer = new Three.WebGLRenderer({ animate: false });
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      document.body.appendChild(this.renderer.domElement)
+
+    },
+  },
+};
+</script>
+
+<style scoped></style>
